55f0ddd00c2041542c20d9324a197ed05fc71776
Fredrik Arvidsson <fresa@fresa.se>
2018-07-10 23:20:25 +0200

    cleaned up some unused code

--- src/Test.It.While.Hosting.Your.Web.Application/HostStarters/DefaultWebApplicationHostStarter.cs
+++ src/Test.It.While.Hosting.Your.Web.Application/HostStarters/DefaultWebApplicationHostStarter.cs
@@ -38,47 +38,14 @@ namespace Test.It.While.Hosting.Your.Web.Application.HostStarters
 #endif
 
 #if NETCOREAPP2_1
-using System;
 using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Test.It.Specifications;
-using Test.It.While.Hosting.Your.Web.Application.ApplicationBuilders;
 
 namespace Test.It.While.Hosting.Your.Web.Application.HostStarters
 {
-    public class DefaultWebApplicationHostStarter2<TApplicationBuilder> : IWebApplicationHostStarter
-        where TApplicationBuilder : IWebApplicationBuilder, new()
-    {
-        private TestServer _testServer;
-
-        public HttpClient Start(ITestConfigurer testConfigurer)
-        {
-            void Startup(IApplicationBuilder appBuilder)
-            {
-                appBuilder.Properties[OwinProperties.ExceptionHandler] = OnUnhandledException;
-
-                var applicationBuilder = new TApplicationBuilder();
-                applicationBuilder.CreateWith(testConfigurer).Start(appBuilder);
-            }
-
-            var webHostBuilder = new WebHostBuilder().Configure(Startup);
-            _testServer = new TestServer(webHostBuilder);
-
-            return _testServer.CreateClient();
-        }
-
-        public event ExceptionHandler OnUnhandledException;
-
-        public void Dispose()
-        {
-            _testServer?.Dispose();
-        }
-    }
-
     public class DefaultWebApplicationHostStarter<TApplication> : IWebApplicationHostStarter
         where TApplication : class, IStartup
     {
@@ -94,7 +61,7 @@ namespace Test.It.While.Hosting.Your.Web.Application.HostStarters
             var webHostBuilder = new WebHostBuilder()
                 .Configure(Startup)
                 .UseStartup<TApplication>()
-                .ConfigureServices(collection => { testConfigurer.Configure(new CoreServiceContainer(collection)); });
+                .ConfigureServices(collection => { testConfigurer.Configure(new WebApiServiceContainer(collection)); });
             _testServer = new TestServer(webHostBuilder);
 
             return _testServer.CreateClient();
@@ -105,49 +72,6 @@ namespace Test.It.While.Hosting.Your.Web.Application.HostStarters
         public void Dispose()
         {
             _testServer?.Dispose();
-        }
-    }
-
-    internal class CoreServiceContainer : IServiceContainer
-    {
-        private readonly IServiceCollection _serviceCollection;
-
-        private readonly Lazy<IServiceProvider> _serviceProviderResolver;
-        private IServiceProvider ServiceProvider => _serviceProviderResolver.Value;
-
-        public CoreServiceContainer(IServiceCollection serviceCollection)
-        {
-            _serviceCollection = serviceCollection;
-            _serviceProviderResolver = new Lazy<IServiceProvider>(() => _serviceCollection.BuildServiceProvider());
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public void Register<TImplementation>(Func<TImplementation> configurer) where TImplementation : class
-        {
-            _serviceCollection.Replace(ServiceDescriptor.Transient(provider => configurer()));
-        }
-
-        public void Register<TService, TImplementation>() where TService : class where TImplementation : class, TService
-        {
-            _serviceCollection.Replace(ServiceDescriptor.Transient<TService, TImplementation>());
-        }
-
-        public void RegisterSingleton<TImplementation>(Func<TImplementation> configurer) where TImplementation : class
-        {
-            _serviceCollection.Replace(ServiceDescriptor.Singleton(provider => configurer()));
-        }
-
-        public void RegisterSingleton<TService, TImplementation>() where TService : class where TImplementation : class, TService
-        {
-            _serviceCollection.Replace(ServiceDescriptor.Singleton<TService, TImplementation>());
-        }
-
-        public TService Resolve<TService>() where TService : class
-        {
-            return ServiceProvider.GetService<TService>();
         }
     }
 }
---
+++ src/Test.It.While.Hosting.Your.Web.Application/HostStarters/WebApiServiceContainer.cs
@@ -0,0 +1,51 @@
+﻿#if NETCOREAPP2_1
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Test.It.While.Hosting.Your.Web.Application.HostStarters
+{
+    internal class WebApiServiceContainer : IServiceContainer
+    {
+        private readonly IServiceCollection _serviceCollection;
+
+        private readonly Lazy<IServiceProvider> _serviceProviderResolver;
+        private IServiceProvider ServiceProvider => _serviceProviderResolver.Value;
+
+        public WebApiServiceContainer(IServiceCollection serviceCollection)
+        {
+            _serviceCollection = serviceCollection;
+            _serviceProviderResolver = new Lazy<IServiceProvider>(() => _serviceCollection.BuildServiceProvider());
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Register<TImplementation>(Func<TImplementation> configurer) where TImplementation : class
+        {
+            _serviceCollection.Replace(ServiceDescriptor.Transient(provider => configurer()));
+        }
+
+        public void Register<TService, TImplementation>() where TService : class where TImplementation : class, TService
+        {
+            _serviceCollection.Replace(ServiceDescriptor.Transient<TService, TImplementation>());
+        }
+
+        public void RegisterSingleton<TImplementation>(Func<TImplementation> configurer) where TImplementation : class
+        {
+            _serviceCollection.Replace(ServiceDescriptor.Singleton(provider => configurer()));
+        }
+
+        public void RegisterSingleton<TService, TImplementation>() where TService : class where TImplementation : class, TService
+        {
+            _serviceCollection.Replace(ServiceDescriptor.Singleton<TService, TImplementation>());
+        }
+
+        public TService Resolve<TService>() where TService : class
+        {
+            return ServiceProvider.GetService<TService>();
+        }
+    }
+}
+#endif
